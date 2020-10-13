d11251e4b6e0dbf6f8ae8def0d1c01e40eba7eee
fabiansky <fabienkahe@gmail.com>
2020-10-13 16:50:17 +0000

    first commit

---
+++ src/main/java/com/fabien/devopsapp/DevopsappApplication.java
@@ -0,0 +1,13 @@
+package com.fabien.devopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabien/devopsapp/DevopsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabien.devopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
